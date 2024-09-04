abfa169f8fbed59af28194d973dd22622fa54312
ayomide <mikaheelayomide5@gmail.com>
2024-09-04 21:26:40 +0100

    currently working on the navbar

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,7 +1,14 @@
-const Navbar = () {
+import { FiAlignJustify } from "react-icons/fi"
+import './Navbar.css'
+
+const Navbar = () => {
   return(
-    <>
-      
-    </>
+    <div className="Navbar">
+      <h2> S.<span className="o">O</span>.A </h2> 
+      <h3> ELECTRICIAN </h3>
+      <FiAlignJustify />
+    </div>
     )
 }
+
+export default Navbar
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,10 +1,11 @@
-import Navbar from '../Components/Navbar.jsx'
+import Navbar from '../Layout/Navbar.jsx'
 
 function Homepage() {
   return(
     <>
       <Navbar />
-    </>
+    </> 
     )
 }
 export default Homepage
+ 
