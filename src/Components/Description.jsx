abe57d952ffd6ba8fc007e49f148d02be74e346c
ayomide <mikaheelayomide5@gmail.com>
2024-09-13 17:47:49 +0100

    done with first description

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -1,22 +1,51 @@
 function Description() {
   
-  const firstdiv = {
-    
+  const background = {
+    backgroundColor: "#f1f1f1",
+    marginTop: "10%"
   }
+  
+  const firstdiv = {
+    fontSize: "1.8rem",
+    textAlign: "center",
+    margin: "10%",
+    paddingTop: "10%"
+  }
+  
+  const secondiv = {
+    marginTop: "10%",
+    fontSize: "1.3rem",
+    textAlign: 'center',
+    margin: "12%"
+  }
+  
+  const gradientTextStyle = {
+   background:'linear-gradient(to right,blue,yellow)',
+   WebkitBackgroundClip: 'text',
+   WebkitTextFillColor: 'transparent'
+};
 
 return(
   <>
+  <div style={background}>
     <div style={firstdiv}>
-      <h2>
-        Electrician that creates a good and better life for customers 
-      </h2>
+      <h3>
+        Electrician that creates a 
+        <span style={gradientTextStyle} > good </span>
+        and
+        <span style={gradientTextStyle}> better life </span> for customers 
+      </h3>
     </div>
    
-    <div className="secondiv"> 
+    <div style={secondiv}> 
       <p>
-        Always ready and at your service 
+        Always ready for all your service at your convenience
       </p>
     </div>
+    
+  </div>
+  
+  
   </>
   )
 }
--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -4,9 +4,9 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return(
     <div className={styles.Navbar}>
-      <h1> S.<span className={styles.o}>O</span>.A </h1> 
+      <h1 className={styles.soa}> S.<span className={styles.o}>O</span>.A </h1> 
       
-      <h2> E<span className={styles.o}>LE</span>CTR<span className={styles.o}>ICI</span>AN 
+      <h2 className={styles.theh2}> E<span className={styles.o}>LE</span>CTR<span className={styles.o}>ICI</span>AN 
       </h2>
       
       <FiAlignJustify style={{fontSize:"30px"}} />
